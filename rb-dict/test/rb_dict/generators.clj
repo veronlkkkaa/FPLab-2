3e440769f36b9d75ea35897979d49854ccfc9e48
veronlkkkaa <veronlkkkaaa@mail.ru>
2026-01-12 17:04:03 +0300

    add changes

--- rb-dict/test/rb_dict/generators.clj
+++ rb-dict/test/rb_dict/generators.clj
@@ -1,5 +1,6 @@
 (ns rb-dict.generators
-  (:require [clojure.test.check.generators :as gen]))
+  (:require [clojure.test.check.generators :as gen]
+            [rb-dict.core :as dict]))
 
 (def gen-int gen/int)
 
@@ -8,3 +9,19 @@
 
 (def gen-pairs
   (gen/vector gen-pair 0 20))
+
+;; Генератор с использованием gen/bind:
+;; Создаёт словарь и ключ, который гарантированно в нём есть
+(def gen-dict-with-key
+  (gen/bind gen-pairs
+    (fn [pairs]
+      (if (empty? pairs)
+        ;; Если пар нет, возвращаем пустой словарь и nil
+        (gen/return {:dict dict/empty-dict :key nil :has-key false})
+        ;; Если пары есть, строим словарь и выбираем случайный существующий ключ
+        (let [d (reduce (fn [d [k v]] (dict/insert d k v))
+                        dict/empty-dict
+                        pairs)
+              keys (map first pairs)]
+          (gen/fmap (fn [k] {:dict d :key k :has-key true})
+                    (gen/elements keys)))))))
